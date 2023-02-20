ecf5d26a0f76a19c385a83c2f1177acf7ed0ce5b
Thomas Brockington <xtombrock1989@gmail.com>
2023-02-20 22:42:12 +0000

    item added

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import CTA from '../../components/cta/CTA';
 import Item from '../../components/item/Item';
 // Components
@@ -8,13 +8,30 @@ import TotalOwed from '../../components/totalOwed/TotalOwed'
 import './home.css';
 
 function Home() {
+  const [itemsDisplay, setItemsDisplay] = useState([])
+  console.log('itemsDisplay', itemsDisplay);
+
+  useEffect(() => {
+    console.log('using an effect to get post query');
+    fetch(`http://localhost:4000/items`)
+      .then((res) => res.json())
+      .then((data) => {
+        setItemsDisplay(data.data);
+        console.log('data: ', data.data);
+      })
+      .catch((error) => {
+        console.log('error', error);
+      });
+  }, []);
+
   return (
     <>
       <section className='homepage__container'>
         <Navbar />
         <div className="main__container">
             <TotalOwed />
-            <Item />
+            {itemsDisplay.length > 0 ? <Item /> : <div>Nothing to display...</div>}
+            
             <CTA />
         </div>
       </section>
