ed57cdb2e98bd4d53ab67633215736145202037d
Thomas Brockington <xtombrock1989@gmail.com>
2023-02-20 19:46:46 +0000

    init

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,16 @@
+import { Routes, Route } from 'react-router-dom';
+import Error404 from './pages/error/Error404';
+import Home from './pages/home/Home';
 
 function App() {
   return (
     <>
-      Hello World
+      <Routes>
+
+        <Route path='/' element={<Home />} />
+        <Route path='8' element={<Error404 />} />
+
+      </Routes>
     </>
   );
 }
