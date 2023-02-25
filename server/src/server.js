f4c019e544c612e692e1ca5cb3e013ad64989a46
Thomas Brockington <xtombrock1989@gmail.com>
2023-02-25 23:48:16 +0000

    server may work

--- server/src/server.js
+++ server/src/server.js
@@ -1,5 +1,10 @@
+// Load our .env file
+require('dotenv').config();
+
 const express = require('express');
 const app = express();
+// Used for __dirname
+const path = require('path')
 
 const cors = require('cors');
 const morgan = require('morgan');
@@ -11,22 +16,37 @@ app.use(cors());
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'))
 
-app.get('*', (req, res) => {
-    res.status(404).json({
-      status: 'fail',
-      data: {
-        resource: 'Not found 404 error'
-      }
-    })
-  })
+
+// Set the port and URl
+const PORT = process.env.PORT || 4000;
+const URL = process.env.URL || 'http://localhost:'
 
 // Tell express to use your routers here
+// const userRouter = require('./routes/usersEX');
 const userRouter = require('./routes/users');
-const itemRouter = require('./routes/items')
 
-app.use('/items', itemRouter);
 app.use('/users', userRouter);
-app.use('/', userRouter);
+// app.use('/', userRouter);
 
-module.exports = app
+app.get('/', (req, res) => {
+    res.sendFile('index.html', {root: path.join(__dirname, '..', 'public')});
+})
+
+// For all unknown requests 404 page returns
+app.all('*', (req, res) => {
+    res.status(404)
+    if (req.accepts('html')) {
+        res.sendFile(path.join(__dirname, '..', 'public', 'error404.html'))
+    } else if (req.accepts('json')) {
+        res.json({ message: '404 Not Found' })
+    } else {
+        res.type('txt').send('404 Not Found')
+    }
+})
+
+// Start our API server
+app.listen(PORT, () => {
+    console.log(`\nServer is running on ${URL}${PORT} - this no longer consumes souls\n`);
+});
