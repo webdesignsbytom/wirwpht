a16b0b2d727a9e08806d49e76f5cbd8e2d71c516
xtombrock <xtombrock1989@gmail.com>
2023-04-08 18:42:54 +0100

    save

--- client/src/index.js
+++ client/src/index.js
@@ -3,10 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './styles/index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
+import UserContextProvider from './context/UserContext';
+import ToggleContextProvider from './context/ToggleContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+  <BrowserRouter>
+    <UserContextProvider>
+      <ToggleContextProvider>
+        <App />
+      </ToggleContextProvider>
+    </UserContextProvider>
+  </BrowserRouter>
 );
